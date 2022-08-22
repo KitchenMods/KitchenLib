549ec084fe51d8daeb77ecb23d51221a43d6216b
Peter Corcoran <peter.corcoran@shadowacre.ltd>
2022-08-23 00:25:50 +0100

    A nice green colour for the mod via MelonLoader.

--- KitchenLib/Properties/AssemblyInfo.cs
+++ KitchenLib/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using MelonLoader;
 [assembly: MelonInfo(typeof(KitchenLib.Mod), "KitchenLib", "0.1.0", "KitchenMods")]
 [assembly: MelonGame("It's Happening", "PlateUp")]
 [assembly: MelonPriority(-1000000)]
+[assembly: MelonColor(System.ConsoleColor.Green)]
