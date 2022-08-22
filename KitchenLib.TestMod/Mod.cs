dbd40e2a4f85974e5cdaef3d726746b64dcee41c
Peter Corcoran <peter.corcoran@shadowacre.ltd>
2022-08-22 22:15:13 +0100

    Reverted to registering appliance in OnApplicationStart as the MelonPriority doesn't work correctly.

--- KitchenLib.TestMod/Mod.cs
+++ KitchenLib.TestMod/Mod.cs
@@ -5,6 +5,11 @@ namespace KitchenLib.TestMod
 	public class Mod : BaseMod
 	{
 		public Mod() : base() {
+			
+		}
+		
+		public override void OnApplicationStart() {
+			base.OnApplicationStart();
 			RegisterCustomAppliance(new CustomApplianceInfo() {
 				Name            = "Testing Terminal",
 				Description     = "This is how we test things, from a terminal!"
--- KitchenLib/Properties/AssemblyInfo.cs
+++ KitchenLib/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using MelonLoader;
 
 [assembly: MelonInfo(typeof(KitchenLib.Mod), "KitchenLib", "0.1.0", "KitchenMods")]
 [assembly: MelonGame("It's Happening", "PlateUp")]
+[assembly: MelonPriority(-1000000)]
