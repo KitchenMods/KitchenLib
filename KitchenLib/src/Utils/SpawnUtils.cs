7ae879828c6c05b4d8b4658044e8fd5b713d1f05
Peter Corcoran <peter.corcoran@shadowacre.ltd>
2022-08-24 02:20:26 +0100

    Added alternative calling pattern for SpawnUtil.SpawnApplianceBlueprint that allows spawning by type, avoiding ID requirements.

--- KitchenLib/src/Utils/SpawnUtils.cs
+++ KitchenLib/src/Utils/SpawnUtils.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using UnityEngine;
 using Unity.Entities;
+using KitchenLib.Appliances;
 
 namespace KitchenLib.Utils
 {
@@ -11,6 +12,13 @@ namespace KitchenLib.Utils
 			var em = Unity.Entities.World.DefaultGameObjectInjectionWorld.GetExistingSystem<Kitchen.PlayerManager>().EntityManager;
 			var position = GameObject.Find("Player(Clone)").transform.position;
 			return Kitchen.PostHelpers.CreateBlueprintLetter(em, position, id, priceModifier, forcePrice);
+		}
+
+		public static Entity SpawnApplianceBlueprint<T>(float priceModifier = 0f, int forcePrice = -1) where T : CustomAppliance {
+			var appliance = CustomAppliances.Get<T>();
+			if(appliance == null)
+				return default(Entity);
+			return SpawnApplianceBlueprint(appliance.ID, priceModifier, forcePrice);
 		}
 	}
 }
