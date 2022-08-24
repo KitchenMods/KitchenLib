ea3f4b9bcd1e433c7d73c9636b6195dd244ad078
StarFluxGames <starfluxgames@gmail.com>
2022-08-24 14:06:07 +1000

    Tweaking

--- KitchenLib.TestMod/Mod.cs
+++ KitchenLib.TestMod/Mod.cs
@@ -1,14 +1,31 @@
-﻿using KitchenLib.Appliances;
+﻿using Kitchen;
+using KitchenLib.Appliances;
+using KitchenLib.Utils;
+using UnityEngine;
+using HarmonyLib;
 
 namespace KitchenLib.TestMod
 {
 	public class Mod : BaseMod
 	{
 		public Mod() : base() { }
-		
+        public static CustomAppliance d;
 		public override void OnApplicationStart() {
 			base.OnApplicationStart();
-			AddAppliance<TestingTerminalAppliance>();
-		}
-	}
+			d = AddAppliance<TestingTerminalAppliance>();
+        }
+        [HarmonyPatch(typeof(ProvideStartingEnvelopes), "OnUpdate")]
+        class ProvideStartingEnvelopes_Patch
+        {
+            [HarmonyPostfix]
+            static void Postfix(ProvideStartingEnvelopes __instance)
+            {
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    SpawnUtils.SpawnApplianceBlueprint(Mod.d.ID, 0f, -1);
+                }
+            }
+
+        }
+    }
 }
