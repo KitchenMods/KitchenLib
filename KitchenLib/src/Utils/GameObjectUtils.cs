96e5ac7cb1c32e453d1ba7239f082ed48165780f
Lachlan Leone <starfluxgames@gmail.com>
2022-09-06 02:44:08 +1000

    Added caching for FieldInfo, Added a GameObjectUtils.GetChildObject allowing for easier child finding

---
+++ KitchenLib/src/Utils/GameObjectUtils.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KitchenLib.Utils
+{
+	public class GameObjectUtils
+	{
+		public static GameObject GetChildObject(GameObject prefab, string path)
+		{
+			var currentRef = prefab.transform;
+			var splitPath = path.Split('/');
+			foreach (var part in splitPath)
+				currentRef = currentRef?.Find(part);
+
+			if (currentRef != null)
+				return currentRef.gameObject;
+			else
+				return null;
+		}
+	}
+}
--- KitchenLib/src/Utils/ReflectionUtils.cs
+++ KitchenLib/src/Utils/ReflectionUtils.cs
@@ -8,6 +8,8 @@ namespace KitchenLib.Utils
 	{
 		private static Dictionary<(Type, string), MethodInfo> cachedMethods = new Dictionary<(Type, string), MethodInfo>();
 
+		public static Dictionary<(Type, string), FieldInfo> cachedFields = new Dictionary<(Type, string), FieldInfo>();
+
 		public static MethodInfo GetMethod<T>(string methodName, BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance)
 		{
 			var tuple = (typeof(T), methodName);
@@ -15,6 +17,14 @@ namespace KitchenLib.Utils
 				return cachedVal;
 			cachedMethods[tuple] = typeof(T).GetMethod(methodName, flags);
 			return cachedMethods[tuple];
+		}
+		public static FieldInfo GetField<T>(string fieldName, BindingFlags flags = BindingFlags.NonPublic | BindingFlags.Instance)
+		{
+			var tuple = (typeof(T), fieldName);
+			if (cachedFields.TryGetValue(tuple, out var cachedVal))
+				return cachedVal;
+			cachedFields[tuple] = typeof(T).GetField(fieldName, flags);
+			return cachedFields[tuple];
 		}
 	}
 }
