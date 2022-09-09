1420de48a1c77af1e1984cca48b022a51613cc63
Lachlan Leone <starfluxgames@gmail.com>
2022-09-10 00:43:13 +1000

    Changed name of SpawnApplianceBlueprint

--- KitchenLib/src/Utils/SpawnUtils.cs
+++ KitchenLib/src/Utils/SpawnUtils.cs
@@ -6,16 +6,17 @@ namespace KitchenLib.Utils
 {
 	public class SpawnUtils
 	{
-		public static Entity SpawnApplianceBlueprint(int id, float priceModifier = 0f, int forcePrice = -1) {
+		public static Entity SpawnApplianceBlueprintAtPlayer(int id, float priceModifier = 0f, int forcePrice = -1) {
 			var position = GameObject.Find("Player(Clone)").transform.position;
 			return Kitchen.PostHelpers.CreateBlueprintLetter(EntityUtils.GetEntityManager(), position, id, priceModifier, forcePrice);
 		}
 
-		public static Entity SpawnApplianceBlueprint<T>(float priceModifier = 0f, int forcePrice = -1) where T : CustomAppliance {
+		public static Entity SpawnApplianceBlueprintAtPlayer<T>(float priceModifier = 0f, int forcePrice = -1) where T : CustomAppliance {
 			var appliance = CustomAppliances.Get<T>();
 			if(appliance == null)
 				return default(Entity);
-			return SpawnApplianceBlueprint(appliance.ID, priceModifier, forcePrice);
+			return SpawnApplianceBlueprintAtPlayer(appliance.ID, priceModifier, forcePrice);
 		}
+
 	}
 }
