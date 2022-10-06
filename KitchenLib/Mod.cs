0dcecc3a50235857173c1f790ac5cf7a910eb563
Lachlan Leone <starfluxgames@gmail.com>
2022-10-06 19:37:58 +1100

    Set compatible version 1.0.5

--- KitchenLib/Mod.cs
+++ KitchenLib/Mod.cs
@@ -2,7 +2,7 @@ namespace KitchenLib
 {
 	public class Mod : BaseMod
 	{
-		public Mod() : base("kitchenlib", ">=1.0.0 <=1.0.5") { }
+		public Mod() : base("kitchenlib", "1.0.5") { }
 		public override void OnSceneWasLoaded(int buildIndex, string sceneName) {
 		}
   }
