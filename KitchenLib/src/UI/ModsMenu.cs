96b3b7d05dd2351323737e19f2e5a71679fe2c76
Lachlan Leone <starfluxgames@gmail.com>
2022-11-02 12:16:20 +1100

    Added Non-KitchenLib Mods to Mods Menu

--- KitchenLib/src/UI/ModsMenu.cs
+++ KitchenLib/src/UI/ModsMenu.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 using Kitchen;
 using Kitchen.Modules;
 using KitchenLib.Registry;
+using System.Collections.Generic;
+
 
 namespace KitchenLib
 {
     public partial class ModsMenu<T> : Menu<T>
     {
         public ModsMenu(Transform container, ModuleList module_list) : base(container, module_list) { }
+        private List<string> modNames = new List<string>();
 
         public override void Setup(int player_id) {
             AddLabel("Loaded Mods");
@@ -15,7 +18,10 @@ namespace KitchenLib
             {
                 if (mod.ModName != null && mod.ModVersion != null)
                     if (ModRegistery.isModSafeForVersion(mod))
+                    {
                         AddInfo(mod.ModName + "     v" + mod.ModVersion);
+                        modNames.Add(mod.ModName);
+                    }
             }
 
             AddLabel("Untested Mods");
@@ -23,10 +29,21 @@ namespace KitchenLib
             {
                 if (mod.ModName != null && mod.ModVersion != null)
                     if (!ModRegistery.isModSafeForVersion(mod))
+                    {
                         AddInfo(mod.ModName + "     v" + mod.ModVersion);
+                        modNames.Add(mod.ModName);
+                    }
             }
 
-
+            AddLabel("Non-KitchenLib Mods");
+            System.Collections.ObjectModel.ReadOnlyCollection<MelonLoader.MelonMod> mods = MelonLoader.MelonMod.RegisteredMelons;
+			foreach (MelonLoader.MelonMod mod in mods)
+			{
+                if (!modNames.Contains(mod.Info.Name))
+                {
+                    AddInfo(mod.Info.Name + "     v" + mod.Info.Version);
+                }
+			}
             New<SpacerElement>(true);
             New<SpacerElement>(true);
             AddButton(base.Localisation["MENU_BACK_SETTINGS"], delegate(int i)
