d6e7808ee851348b04379295b67b03b5431963ed
Lachlan Leone <starfluxgames@gmail.com>
2022-12-18 09:47:35 +1100

    Unfinished Remote Commands

---
+++ KitchenLib/src/EntityManagerPassView.cs
@@ -0,0 +1,46 @@
+﻿using Kitchen;
+using Unity.Entities;
+using System.Collections.Generic;
+using Unity.Collections;
+using System;
+
+namespace KitchenLib
+{
+	public class EntityManagerPassView : ResponsiveViewSystemBase<EntityManagerPassView.ViewData, EntityManagerPassView.ResponceData>
+	{
+		protected override void OnUpdate()
+		{
+			var Entities = GetEntityQuery(new QueryHelper()
+					.All(typeof(EntityManagerPassView.ViewData)));
+			foreach (var entity in Entities.ToEntityArray(Allocator.TempJob))
+			{
+				CLinkedView linkedView;
+				Require<CLinkedView>(entity, out linkedView);
+
+				ApplyUpdates(linkedView.Identifier, new Action<EntityManagerPassView.ResponceData>(delegate (EntityManagerPassView.ResponceData data)
+				{
+				}), true);
+			}
+		}
+
+		public struct ViewData : IViewData, IViewResponseData, IViewData.ICheckForChanges<EntityManagerPassView.ViewData>, IRollUp
+		{
+			public IUpdatableObject GetRelevantSubview(IObjectView view)
+			{
+				return view.GetSubView<CrateView>();
+			}
+
+			public bool IsChangedFrom(EntityManagerPassView.ViewData check)
+			{
+				return false;
+			}
+
+		}
+
+		public struct ResponceData : IResponseData, IViewResponseData, IRollUp
+		{
+			public List<string> commands;
+		}
+	}
+
+}
