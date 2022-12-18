5dba0b3ddd3e0625327a07612256272c6fa29149
Lachlan Leone <starfluxgames@gmail.com>
2022-12-18 15:46:48 +1100

    CommandManager

---
+++ KitchenLib/src/Commands/CommandsView.cs
@@ -0,0 +1,64 @@
+﻿using Kitchen;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenLib.Commands
+{
+	public class CommandManager : ResponsiveObjectView<CommandManager.ViewData, CommandManager.ResponseData>
+	{
+		protected override void UpdateData(ViewData data)
+		{
+		}
+
+		public class UpdateView : ResponsiveViewSystemBase<CommandManager.ViewData, CommandManager.ResponseData>
+		{
+			protected override void OnUpdate()
+			{
+				var entities = GetEntityQuery(new QueryHelper().All(typeof(CLinkedView))).ToEntityArray(Allocator.TempJob);
+
+				foreach (Entity entity in entities)
+				{
+					if (Require<CLinkedView>(entity, out CLinkedView linkedView))
+					{
+						ApplyUpdates(linkedView.Identifier, (data) =>
+						{
+							//Do something with data.Commands
+						}, false);
+					}
+				}
+			}
+		}
+
+		public override bool HasStateUpdate(out IResponseData state)
+		{
+			state = null;
+			bool result;
+			if (Response.Commands != null)
+			{
+				state = Response;
+				Response = default(CommandManager.ResponseData);
+				result = true;
+			}
+			else
+			{
+				result = false;
+			}
+			return result;
+		}
+
+		private CommandManager.ResponseData Response;
+
+		public struct ViewData : IViewData, IViewResponseData, IViewData.ICheckForChanges<CommandManager.ViewData>
+		{
+			public bool IsChangedFrom(CommandManager.ViewData check)
+			{
+				return true;
+			}
+		}
+		public struct ResponseData : IResponseData, IViewResponseData
+		{
+			public Dictionary<int, string> Commands;
+		}
+	}
+}
