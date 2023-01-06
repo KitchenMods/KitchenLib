6407bf7e5a69f2972a1fb820aa0384cc3ff72f3d
Lachlan Leone <starfluxgames@gmail.com>
2023-01-07 09:51:36 +1100

    v0.3.7

--- KitchenLib/src/Event/Events.cs
+++ KitchenLib/src/Event/Events.cs
@@ -18,9 +18,11 @@ namespace KitchenLib.Event
         public static EventHandler<MainMenu_SetupArgs> MainMenu_SetupEvent;
         public static EventHandler<PlayerPauseView_SetupMenusArgs> PlayerPauseView_SetupMenusEvent;
 
-        //Client
-        public static EventHandler<PreferenceMenu_SetupArgs> PreferenceMenu_MainMenu_SetupEvent;
-        public static EventHandler<PreferenceMenu_SetupArgs> PreferenceMenu_PauseMenu_SetupEvent;
+		//Client
+		[Obsolete("Please use ModsPreferencesMenu<T>.RegisterMenu")]
+		public static EventHandler<PreferenceMenu_SetupArgs> PreferenceMenu_MainMenu_SetupEvent;
+		[Obsolete("Please use ModsPreferencesMenu<T>.RegisterMenu")]
+		public static EventHandler<PreferenceMenu_SetupArgs> PreferenceMenu_PauseMenu_SetupEvent;
         public static EventHandler<PreferenceMenu_CreateSubmenusArgs<MainMenuAction>> PreferenceMenu_MainMenu_CreateSubmenusEvent;
         public static EventHandler<PreferenceMenu_CreateSubmenusArgs<PauseMenuAction>> PreferenceMenu_PauseMenu_CreateSubmenusEvent;
     }
--- KitchenLib/src/UI/ModsPreferencesMenu.cs
+++ KitchenLib/src/UI/ModsPreferencesMenu.cs
@@ -6,6 +6,9 @@ using System;
 using System.Reflection;
 using KitchenLib.Event;
 using KitchenLib.Utils;
+using System.Runtime.CompilerServices;
+using Harmony;
+using System.Linq.Expressions;
 
 namespace KitchenLib
 {
@@ -13,35 +16,135 @@ namespace KitchenLib
     {
         public ModsPreferencesMenu(Transform container, ModuleList module_list) : base(container, module_list) { }
 
-        private static Dictionary<string, List<BasePreference>> ModIDS = new Dictionary<string, List<BasePreference>>();
+		private static Dictionary<(Type, Type), string> RegisteredMenus = new Dictionary<(Type, Type), string>();
+		private static Dictionary<(Type, Type), int> MenuPages = new Dictionary<(Type, Type), int>();
 
-        public override void Setup(int player_id) {
-            AddLabel("Mod Preferences");
-            New<SpacerElement>(true);
+		private static int MenusPerPage = 5;
+
+		private static Dictionary<(Type, int), int> MenuPageCounter = new Dictionary<(Type, int), int>();
+
+		private static Dictionary<Type, List<int>> Pages = new Dictionary<Type, List<int>>();
+		private static Dictionary<Type, List<string>> PageNames = new Dictionary<Type, List<string>>();
+		
+		private Option<int> PageSelector = null;
+		
+		private static int CurrentPage = 0;
+		
+
+		public static void RegisterMenu(string name, Type type, Type generic)
+		{
+			if (!RegisteredMenus.ContainsKey((type, generic)))
+			{
+				if (!Pages.ContainsKey(generic))
+					Pages.Add(generic, new List<int>());
+				if (!PageNames.ContainsKey(generic))
+					PageNames.Add(generic, new List<string>());
+				
+				bool foundValidPage = false;
+				int page = 0;
+				RegisteredMenus.Add((type, generic), name);
+
+				while (!foundValidPage)
+				{
+					if (MenuPageCounter.ContainsKey((generic, page)))
+					{
+						int pageCount = MenuPageCounter[(generic, page)];
+						if (pageCount < MenusPerPage)
+						{
+							MenuPageCounter[(generic, page)]++;
+							foundValidPage = true;
+						}
+						else
+						{
+							page++;
+						}
+					}
+					else
+					{
+						MenuPageCounter.Add((generic, page), 0);
+						MenuPageCounter[(generic, page)]++;
+						foundValidPage = true;
+						break;
+					}
+				}
+
+				if (foundValidPage)
+				{
+
+					if (!Pages[generic].Contains(page))
+					{
+						Pages[generic].Add(page);
+						PageNames[generic].Add("Page: " + page);
+					}
+					MenuPages.Add((type, generic), page);
+				}
+			}
+		}
+		private static void RegisterLegacyMenu(string name, Type type, bool skip_stack)
+		{
+			RegisterMenu(name, type, type.GetGenericArguments()[0]);
+		}
+
+		private bool HasBeenSetup = false;
+        public override void Setup(int player_id)
+		{
+			if (!HasBeenSetup)
+			{
+				MethodInfo mInfo = ReflectionUtils.GetMethod<ModsPreferencesMenu<T>>("RegisterLegacyMenu");
+				EventUtils.InvokeEvent(nameof(Events.PreferenceMenu_MainMenu_SetupEvent), Events.PreferenceMenu_MainMenu_SetupEvent?.GetInvocationList(), null, new PreferenceMenu_SetupArgs(this, mInfo));
+				EventUtils.InvokeEvent(nameof(Events.PreferenceMenu_PauseMenu_SetupEvent), Events.PreferenceMenu_PauseMenu_SetupEvent?.GetInvocationList(), null, new PreferenceMenu_SetupArgs(this, mInfo));
+				HasBeenSetup = true;
+			}
+			CurrentPage = 0;
+			if (!Pages.ContainsKey(GetType().GetGenericArguments()[0]))
+				Pages.Add(GetType().GetGenericArguments()[0], new List<int>());
+			if (!PageNames.ContainsKey(GetType().GetGenericArguments()[0]))
+				PageNames.Add(GetType().GetGenericArguments()[0], new List<string>());
 			
-			MethodInfo mInfo = ReflectionUtils.GetMethod<ModsPreferencesMenu<T>>("AddSubmenuButton");
-            if (this.GetType().GetGenericArguments()[0] == typeof(MainMenuAction))
-                EventUtils.InvokeEvent(nameof(Events.PreferenceMenu_MainMenu_SetupEvent), Events.PreferenceMenu_MainMenu_SetupEvent?.GetInvocationList(), null, new PreferenceMenu_SetupArgs(this, mInfo));
-            else if (this.GetType().GetGenericArguments()[0] == typeof(PauseMenuAction))
-                EventUtils.InvokeEvent(nameof(Events.PreferenceMenu_PauseMenu_SetupEvent), Events.PreferenceMenu_PauseMenu_SetupEvent?.GetInvocationList(), null, new PreferenceMenu_SetupArgs(this, mInfo));
+			PageSelector = new Option<int>(Pages[GetType().GetGenericArguments()[0]], CurrentPage, PageNames[GetType().GetGenericArguments()[0]]);
+			PageSelector.OnChanged += delegate (object _, int result)
+			{
+				CurrentPage = result;
+				Redraw(CurrentPage);
+			};
 
-            New<SpacerElement>(true);
-            New<SpacerElement>(true);
-            AddButton(base.Localisation["MENU_BACK_SETTINGS"], delegate(int i)
+			Redraw(CurrentPage);
+		}
+
+		private void Redraw(int pageNumber = 0)
+		{
+			ModuleList.Clear();
+			
+			AddLabel("Mod Preferences");
+			New<SpacerElement>(true);
+
+			if (Pages[GetType().GetGenericArguments()[0]].Count >= 2)
+			{
+				AddSelect<int>(PageSelector);
+			}
+
+			New<SpacerElement>(true);
+
+			foreach ((Type, Type) menu in RegisteredMenus.Keys)
+			{
+				if (menu.Item2 == this.GetType().GetGenericArguments()[0])
+				{
+					if (MenuPages[menu] == pageNumber)
+					{
+						AddSubmenuButton(RegisteredMenus[menu], menu.Item1, false);
+					}
+				}
+			}
+
+			New<SpacerElement>(true);
+			New<SpacerElement>(true);
+			AddButton(base.Localisation["MENU_BACK_SETTINGS"], delegate (int i)
 			{
 				this.RequestPreviousMenu();
 			}, 0, 1f, 0.2f);
-        }
+		}
 
-        public void AddNewButton(Type type, string name)
-        {
-            AddButton(name, delegate
-            {
-                RequestSubMenu(type);
-            });
-        }
-
-        public override void CreateSubmenus(ref Dictionary<Type, Menu<T>> menus)
+		public override void CreateSubmenus(ref Dictionary<Type, Menu<T>> menus)
         {
             if (this.GetType().GetGenericArguments()[0] == typeof(MainMenuAction))
                 EventUtils.InvokeEvent(nameof(Events.PreferenceMenu_MainMenu_CreateSubmenusEvent), Events.PreferenceMenu_MainMenu_CreateSubmenusEvent?.GetInvocationList(), null, new PreferenceMenu_CreateSubmenusArgs<T>(this, menus, this.Container, this.ModuleList));
