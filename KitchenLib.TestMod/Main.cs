38b777db66589ad381e85810beebc6eb5a4f8dac
Lachlan Leone <starfluxgames@gmail.com>
2023-02-08 18:58:14 +1100

    Remove testing mod

--- KitchenLib.TestMod/Main.cs
+++ KitchenLib.TestMod/Main.cs
@@ -6,27 +6,7 @@ using KitchenMods;
 
 namespace KitchenLib.TestMod
 {
-	public class Main : BaseMod
-	{
-		public Main() : base("kitchenlib.testmod", "KitchenLib TestMod", "KitchenMods", "0.1.0", "1.1.1", Assembly.GetExecutingAssembly()) { }
-
-		protected override void OnFrameUpdate()
-		{
-			throw new NotImplementedException();
-		}
-
-		protected override void OnInitialise()
-		{
-			throw new NotImplementedException();
-		}
-
-		public static CustomAppliance d;
-		public static CustomItem sushiRoll;
-
-		public static CustomProcess rollProcess;
-
-		public static AssetBundle bundle;
- 
+	public class Main {
     }
 	/*
 	* This is a custom AudioClip returned for the custom process 'rollProcess' - This needs to be added to KitchenLib
--- KitchenLib.TestMod/TestingTerminalAppliance.cs
+++ KitchenLib.TestMod/TestingTerminalAppliance.cs
@@ -3,16 +3,4 @@ using KitchenLib.Customs;
 
 namespace KitchenLib.TestMod
 {
-	public class TestingTerminalAppliance : CustomAppliance
-	{
-		public override string Name
-		{
-			get { return "Sushi Provider"; }
-		}
-
-		public override int BaseGameDataObjectID
-		{
-			get { return -13481890; }
-		}
-    }
 }
