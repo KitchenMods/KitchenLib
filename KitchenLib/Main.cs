7e360627d2e6dc3942afe0e09b5321133f90fd1d
Lachlan Leone <starfluxgames@gmail.com>
2023-02-15 23:54:54 +1100

    version bump

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -18,7 +18,7 @@ namespace KitchenLib
 		public const string MOD_NAME = "KitchenLib";
 		public const string MOD_AUTHOR = "KitchenMods";
 		public const string MOD_VERSION = "0.5.2";
-		public const string MOD_BETA_VERSION = "8-Pref";
+		public const string MOD_BETA_VERSION = "RC-1";
 		public const string MOD_COMPATIBLE_VERSIONS = "1.1.3";
 
 		public static AssetBundle bundle;
