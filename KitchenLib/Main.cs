2260e6d84abbcc9666d80e1da8ab857b11b2c8c1
Lachlan Leone <starfluxgames@gmail.com>
2023-03-07 14:32:58 +1100

    v0.5.6 RC-1

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -18,7 +18,7 @@ namespace KitchenLib
 		public const string MOD_NAME = "KitchenLib";
 		public const string MOD_AUTHOR = "KitchenMods";
 		public const string MOD_VERSION = "0.5.6";
-		public const string MOD_BETA_VERSION = "4";
+		public const string MOD_BETA_VERSION = "RC-1";
 		public const string MOD_COMPATIBLE_VERSIONS = "1.1.4";
 
 		public static AssetBundle bundle;
