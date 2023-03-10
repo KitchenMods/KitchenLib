c4c9f97b96b30741a69e559534b6da812b15b27f
Matthew McCaskill <mattrmccaskill@gmail.com>
2023-03-10 13:02:05 -0600

    Fixed namespace of ViewUnionAttributePatch

--- KitchenLib/src/Patches/ViewUnionAttributePatches.cs
+++ KitchenLib/src/Patches/ViewUnionAttributePatches.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 
-namespace KitchenLib.src.Patches
+namespace KitchenLib.Patches
 {
     [HarmonyPatch]
     internal class UnionAttributePatch
