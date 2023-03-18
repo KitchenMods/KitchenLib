08e680f3ae4ce225016b84f144e6fe05fd5f2556
Zoey D <zdavis8529@gmail.com>
2023-03-17 20:03:16 -0600

    Grant Necessary Fixes and API
    Allows the user to add their own necessity (e.g. different types of cups that are in short supply) and also disabling plates if they are not needed in a run.

--- KitchenLib/src/Utils/GDOUtils.cs
+++ KitchenLib/src/Utils/GDOUtils.cs
@@ -67,6 +67,15 @@ namespace KitchenLib.Utils
 			return (T)GetCustomGameDataObject(modID, name)?.GameDataObject;
 		}
 
+		public static List<int> AutomatedParcelItems = new();
+		public static void AddAutomatedParcel(Item item)
+		{
+			if (item.DedicatedProvider == null)
+				return;
+			if (!AutomatedParcelItems.Contains(item.ID))
+				AutomatedParcelItems.Add(item.ID);
+		}
+
 
 		public static Dictionary<int, List<int>> BlacklistedDishSides = new Dictionary<int, List<int>>();
 		public static void BlacklistSide(Item item, int side)
