6bf8043d4d00c985d3f93ebacf58750b66771e6a
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-03-19 15:24:12 +0800

    Fix for ObjectSplittableView not updating correctly when SplitDepletedItem is splittable

---
+++ KitchenLib/src/Patches/ItemExtensions_Patch.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenData;
+using Unity.Entities;
+using KitchenLib.Systems;
+
+namespace KitchenLib.Patches
+{
+	[HarmonyPatch(typeof(ItemExtensions), nameof(ItemExtensions.ChangeItemType))]
+	static class ItemExtensions_Patch
+	{
+		static void Postfix(Entity item, int new_type)
+		{
+			if (GameData.Main.TryGet<Item>(new_type, out var output, warn_if_fail: true) && output.IsSplittable)
+			{
+				EntityManager entityManager = SplittableDepletedDelaySystem.instance.EntityManager;
+
+				SplittableDepletedDelaySystem.CSplittableDepletedDelay delay =
+					new SplittableDepletedDelaySystem.CSplittableDepletedDelay
+					{
+						IsFirstFrame = true,
+						TimeRemaining = 0.1f,
+						SplitCount = output.SplitCount
+					};
+
+				entityManager.AddComponent<SplittableDepletedDelaySystem.CSplittableDepletedDelay>(item);
+				entityManager.SetComponentData(item, delay);
+			}
+		}
+	}
+}
---
+++ KitchenLib/src/Systems/SplittableDepletedDelaySystem.cs
@@ -0,0 +1,65 @@
+﻿using Kitchen;
+using KitchenMods;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace KitchenLib.Systems
+{
+	internal class SplittableDepletedDelaySystem : GameSystemBase, IModSystem
+	{
+		internal struct CSplittableDepletedDelay : IComponentData, IModComponent
+		{
+			public bool IsFirstFrame;
+			public float TimeRemaining;
+			public int SplitCount;
+		}
+
+		EntityQuery Items;
+
+		internal static SplittableDepletedDelaySystem instance;
+
+		protected override void Initialise()
+		{
+			base.Initialise();
+			instance = this;
+			Items = GetEntityQuery(new QueryHelper()
+				.All(typeof(CSplittableItem), typeof(CSplittableDepletedDelay)));
+		}
+		protected override void OnUpdate()
+		{
+			NativeArray<Entity> items = Items.ToEntityArray(Allocator.Temp);
+			NativeArray<CSplittableItem> splits = Items.ToComponentDataArray<CSplittableItem>(Allocator.Temp);
+			NativeArray<CSplittableDepletedDelay> delays = Items.ToComponentDataArray<CSplittableDepletedDelay>(Allocator.Temp);
+
+			float dt = Time.DeltaTime;
+
+			for (int i = 0; i < items.Length; i++)
+			{
+				Entity item = items[i];
+				CSplittableItem split = splits[i];
+				CSplittableDepletedDelay delay = delays[i];
+
+				if (delay.TimeRemaining < 0f)
+				{
+					split.RemainingCount = delay.SplitCount;
+					Set(item, split);
+					EntityManager.RemoveComponent<CSplittableDepletedDelay>(item);
+					continue;
+				}
+
+				if (delay.IsFirstFrame)
+				{
+					delay.IsFirstFrame = false;
+					split.RemainingCount = delay.SplitCount + 1;
+					Set(item, split);
+				}
+				delay.TimeRemaining -= dt;
+				Set(item, delay);
+			}
+
+			items.Dispose();
+			splits.Dispose();
+			delays.Dispose();
+		}
+	}
+}
