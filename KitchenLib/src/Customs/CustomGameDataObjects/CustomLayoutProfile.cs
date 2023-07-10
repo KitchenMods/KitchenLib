7abbe4b8519cb3b1f2d7cfaa990d2c865c296443
Lachlan Leone <starfluxgames@gmail.com>
2023-07-10 15:06:58 +1000

    New LayoutProfile

--- KitchenLib/src/Customs/CustomGameDataObjects/CustomLayoutProfile.cs
+++ KitchenLib/src/Customs/CustomGameDataObjects/CustomLayoutProfile.cs
@@ -1,14 +1,19 @@
 using Kitchen.Layouts;
+using Kitchen.Layouts.Modules;
 using KitchenData;
+using KitchenLib.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using UnityEngine;
+using XNode;
 
 namespace KitchenLib.Customs
 {
     public abstract class CustomLayoutProfile : CustomLocalisedGameDataObject<LayoutProfile, BasicInfo>
     {
+		[Obsolete("Broken")]
         public virtual LayoutGraph Graph { get; protected set; }
         public virtual int MaximumTables { get; protected set; } = 3;
         public virtual List<GameDataObject> RequiredAppliances { get; protected set; } = new List<GameDataObject>();
@@ -18,6 +23,7 @@ namespace KitchenLib.Customs
         public virtual Appliance WallPiece { get; protected set; }
         public virtual Appliance InternalWallPiece { get; protected set; }
         public virtual Appliance StreetPiece { get; protected set; }
+		public virtual List<NodeConnection> NodeConnections { get; protected set; } = new List<NodeConnection>();
 
         [Obsolete("Please set your Name in Info")]
         public virtual string Name { get; protected set; } = "New Layout";
@@ -34,7 +40,15 @@ namespace KitchenLib.Customs
                 result = UnityEngine.Object.Instantiate(gameData.Get<LayoutProfile>().FirstOrDefault(a => a.ID == BaseGameDataObjectID));
 
             if (result.ID != ID) result.ID = ID;
-            if (result.Graph != Graph) result.Graph = Graph;
+			//if (result.Graph != Graph) result.Graph = Graph;
+			
+			
+			result.Graph = new LayoutGraph
+			{
+				nodes = new List<Node>()
+			};
+			
+
             if (result.MaximumTables != MaximumTables) result.MaximumTables = MaximumTables;
             if (result.Info != Info) result.Info = Info;
 
@@ -58,7 +72,9 @@ namespace KitchenLib.Customs
                 }
             }
 
-            gameDataObject = result;
+			PopulateConnections(ref result.Graph, NodeConnections);
+
+			gameDataObject = result;
         }
 
         public override void AttachDependentProperties(GameData gameData, GameDataObject gameDataObject)
@@ -73,5 +89,77 @@ namespace KitchenLib.Customs
             if (result.InternalWallPiece != InternalWallPiece) result.InternalWallPiece = InternalWallPiece;
             if (result.StreetPiece != StreetPiece) result.StreetPiece = StreetPiece;
         }
-    }
+		public struct NodeConnection
+		{
+			public Node FromIndex;
+			public string FromPortName;
+
+			public Node ToIndex;
+			public string ToPortName;
+
+			public NodeConnection(Node fromIndex, Node toIndex, string fromPortName = "Output", string toPortName = "Input")
+			{
+				FromIndex = fromIndex;
+				FromPortName = fromPortName;
+				ToIndex = toIndex;
+				ToPortName = toPortName;
+			}
+		}
+
+		static FieldInfo f_ports = ReflectionUtils.GetField<Node>("ports");
+		private void PopulateConnections(ref LayoutGraph layoutGraph, List<NodeConnection> connections)
+		{
+			foreach (NodeConnection connection in connections)
+			{
+				if (!layoutGraph.nodes.Contains(connection.FromIndex))
+					layoutGraph.nodes.Add(connection.FromIndex);
+				if (!layoutGraph.nodes.Contains(connection.ToIndex))
+					layoutGraph.nodes.Add(connection.ToIndex);
+			}
+			List<Node> nodes = layoutGraph.nodes;
+
+			foreach (Node node in nodes)
+			{
+				if (node is LayoutModule layoutModule)
+				{
+					layoutModule.graph = layoutGraph;
+				}
+			}
+
+			foreach (NodeConnection connection in connections)
+			{
+				if (!TryGetNodePort(connection.FromIndex, connection.FromPortName, out NodePort fromPort))
+					break;
+				if (!TryGetNodePort(connection.ToIndex, connection.ToPortName, out NodePort toPort))
+					break;
+
+				fromPort.Connect(toPort);
+
+				bool TryGetNodePort(Node nodeIndex, string portName, out NodePort nodePort)
+				{
+					nodePort = null;
+					if (!nodes.Contains(nodeIndex))
+					{
+						return false;
+					}
+					Node node = nodeIndex;
+					object obj = f_ports.GetValue(node);
+					if (obj == null || !(obj is Dictionary<string, NodePort> nodeDictionary))
+					{
+						return false;
+					}
+					if (!nodeDictionary.TryGetValue(portName, out nodePort))
+					{
+						return false;
+					}
+					return true;
+				}
+
+				void LogNodeError(object msg)
+				{
+					Main.LogError($"{GetType().FullName} error! {msg}");
+				}
+			}
+		}
+	}
 }
