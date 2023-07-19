ad4ce262d22bafbd3d38939b3f730ad2396b50bf
Yariazen <zicezhao644@gmail.com>
2023-07-18 22:36:50 -0700

    Fixes Details.json not being cleared and Hashes not being generated properly

--- KitchenLib/src/Systems/LogSaveDetails.cs
+++ KitchenLib/src/Systems/LogSaveDetails.cs
@@ -24,62 +24,34 @@ namespace KitchenLib.Systems
 		{
 			Entity entity = this.SaveRequests.First();
 			CRequestSave crequestSave = this.SaveRequests.First<CRequestSave>();
+
 			if (crequestSave.SaveType == SaveType.AutoFull)
 			{
 				EntityContext entityContext = new EntityContext(base.World.EntityManager);
 				string path = Path.Combine(Application.persistentDataPath, "Full", entityContext.Get<SSelectedLocation>().Selected.Slot.ToString());
-				SaveDetails saveDetails = null;
+				SaveDetails saveDetails = new SaveDetails();
 
 				if (!Directory.Exists(path))
 					Directory.CreateDirectory(path);
 
-				if (File.Exists(path + "/details.json"))
-					saveDetails = JsonConvert.DeserializeObject<SaveDetails>(File.ReadAllText(path + "/details.json"));
-
-				if (saveDetails == null)
-					saveDetails = new SaveDetails();
-
-				foreach (string x in GetModList())
-				{
-					if (!saveDetails.Mods.Contains(x))
-					{
-						saveDetails.Mods.Add(x);
-					}
-				}
-				foreach (Entity e in EntityManager.GetAllEntities())
-				{
-					foreach (var component in EntityManager.GetChunk(e).Archetype.GetComponentTypes().Select(c => c.GetManagedType()))
-					{
-						if (!saveDetails.Components.ContainsKey(component.ToString()))
-						{
-							saveDetails.Components.Add(component.ToString(), component.GetHashCode().ToString());
-						}
-					}
-				}
-
-				File.WriteAllText(path + "/details.json", JsonConvert.SerializeObject(saveDetails, Formatting.Indented));
-			}
-		}
-
-		private string[] GetModList()
-		{
-			List<string> nonKlMods = ModPreload.Mods
+				saveDetails.Mods = ModPreload.Mods
 					.Where(mod => mod.State == ModState.PostActivated)
 					.SelectMany(mod => mod.GetPacks<AssemblyModPack>())
 					.Select(pack => pack.Name.Replace(".dll", "")).ToList();
 
-			return nonKlMods.ToArray();
-		}
+				saveDetails.Components = EntityManager
+					.GetAllEntities()
+					.SelectMany(_ => EntityManager.GetChunk(_).Archetype.GetComponentTypes())
+					.ToDictionary(_ => _.GetManagedType().FullName, _ => TypeManager.GetTypeInfo(_.TypeIndex).StableTypeHash.ToString());
 
-		private Dictionary<string, string> GetComponents()
-		{
-			return new Dictionary<string, string>();
+				File.WriteAllText(path + "/details.json", JsonConvert.SerializeObject(saveDetails, Formatting.Indented));
+			}
 		}
 	}
 
 	internal class SaveDetails
 	{
-		public List<string> Mods = new List<string>();
-		public Dictionary<string, string> Components = new Dictionary<string, string>();
+		public List<string> Mods = new();
+		public Dictionary<string, string> Components = new();
 	}
 }
