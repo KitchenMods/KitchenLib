2556678f194b4a0bdf850e3e1c6a2ac6581019f5
Lachlan Leone <starfluxgames@gmail.com>
2023-07-23 21:22:51 +1000

    Prepare for final v0.7.9 release

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -50,7 +50,7 @@ namespace KitchenLib
 		/// <summary>
 		/// The beta version of the mod.
 		/// </summary>
-		internal const string MOD_BETA_VERSION = "RC-4";
+		internal const string MOD_BETA_VERSION = "";
 
 		/// <summary>
 		/// The compatible versions of the mod.
