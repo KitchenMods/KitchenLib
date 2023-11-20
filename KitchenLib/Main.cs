bb9cad0a146e67978029705d12d336a5589f1a34
Lachlan Leone <starfluxgames@gmail.com>
2023-11-21 02:32:54 +1100

    RC1

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -47,7 +47,7 @@ namespace KitchenLib
 		/// <summary>
 		/// The beta version of the mod.
 		/// </summary>
-		internal const string MOD_BETA_VERSION = "7";
+		internal const string MOD_BETA_VERSION = "RC1";
 
 		/// <summary>
 		/// The compatible versions of the mod.
