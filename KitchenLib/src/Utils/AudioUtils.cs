c3e0de2984e4c6e8b0501672be245c63b39db173
Lachlan Leone <starfluxgames@gmail.com>
2024-08-05 23:32:19 +1000

    Obsolete Audio Utils

--- KitchenLib/src/Utils/AudioUtils.cs
+++ KitchenLib/src/Utils/AudioUtils.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,8 +6,10 @@ namespace KitchenLib.Utils
 {
 	public class AudioUtils
 	{
+		[Obsolete]
 		public static Dictionary<int, AudioClip> processAudioClips = new Dictionary<int, AudioClip>();
 
+		[Obsolete]
 		public static bool AddProcessAudioClip(int process, AudioClip clip)
 		{
 			if (!processAudioClips.ContainsKey(process))
@@ -17,6 +20,7 @@ namespace KitchenLib.Utils
 			return false;
 		}
 
+		[Obsolete]
 		public static AudioClip GetProcessAudioClip(int process)
 		{
 			if (processAudioClips.ContainsKey(process))
@@ -26,6 +30,7 @@ namespace KitchenLib.Utils
 			return null;
 		}
 
+		[Obsolete]
 		public static bool DoesProcessAudioClipExist(int process)
 		{
 			return processAudioClips.ContainsKey(process);
