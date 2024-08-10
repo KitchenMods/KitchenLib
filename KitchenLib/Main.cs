abe510bd7512f4659b1b922897681b2d7ed2202e
Lachlan Leone <starfluxgames@gmail.com>
2024-08-10 14:41:18 +1000

    v0.8.3b2

--- KitchenLib/Main.cs
+++ KitchenLib/Main.cs
@@ -50,7 +50,7 @@ namespace KitchenLib
 		/// <summary>
 		/// The beta version of the mod.
 		/// </summary>
-		internal const string MOD_BETA_VERSION = "1";
+		internal const string MOD_BETA_VERSION = "2";
 
 		/// <summary>
 		/// The compatible versions of the mod.
--- KitchenLib/src/Patches/VariousPatches.cs
+++ KitchenLib/src/Patches/VariousPatches.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Reflection;
 using HarmonyLib;
 using Kitchen;
+using KitchenData;
+using KitchenMods;
+using Unity.Entities;
 
 namespace KitchenLib.Patches
 {
@@ -22,4 +26,47 @@ namespace KitchenLib.Patches
 		}
 	}
 	#endregion
+	
+	
+    /*
+     * Thanks to IcedMilo for creating the patch
+     */
+
+    [HarmonyPatch]
+    static class ApplianceComponentHelpers_Patch
+    {
+        [HarmonyTargetMethod]
+        static MethodBase ApplianceComponentSetDynamic_TargetMethod() =>
+            AccessTools.FirstMethod(typeof(ApplianceComponentHelpers), method => method.Name.Contains("SetDynamic") && method.IsGenericMethod).MakeGenericMethod(typeof(IApplianceProperty));
+
+        [HarmonyPrefix]
+        [HarmonyPriority(int.MinValue)]
+        static bool ApplianceComponentSetDynamic_Prefix(bool __runOriginal, EntityContext ctx, Entity e, IApplianceProperty component)
+        {
+            if (!__runOriginal ||
+                !(component is IModComponent))
+                return true;
+            ctx.Set(e, (dynamic)component);
+            return false;
+        }
+    }
+
+    [HarmonyPatch]
+    static class ItemComponentHelpers_Patch
+    {
+        [HarmonyTargetMethod]
+        static MethodBase ItemComponentSetDynamic_TargetMethod() =>
+            AccessTools.FirstMethod(typeof(ItemComponentHelpers), method => method.Name.Contains("SetDynamic") && method.IsGenericMethod).MakeGenericMethod(typeof(IComponentData));
+
+        [HarmonyPrefix]
+        [HarmonyPriority(int.MinValue)]
+        static bool ItemComponentSetDynamic_Prefix(bool __runOriginal, EntityContext ctx, Entity e, IComponentData component)
+        {
+            if (!__runOriginal ||
+                !(component is IModComponent))
+                return true;
+            ctx.Set(e, (dynamic)component);
+            return false;
+        }
+    }
 }
