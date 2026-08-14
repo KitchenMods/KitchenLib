76f0c5a494ebc2901535f1beccb413bc1e5a2c06
Lachlan Leone <lachlan@starfluxgames.com>
2026-08-14 23:31:12 +1000

    Legacy Components
    Moved legacy components into Archive

---
+++ src/Archive/Components/LegacyComponents.cs
@@ -0,0 +1,13 @@
+﻿using KitchenData;
+using Unity.Entities;
+
+namespace KitchenLib.Systems
+{
+	public struct CViewHolder : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CCommandView : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CInfoView : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CSendToClientView : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CTileHightlighterView : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CClientEquipCapeView : IApplianceProperty, IAttachableProperty, IComponentData { }
+	public struct CSyncModsView : IApplianceProperty, IAttachableProperty, IComponentData { }
+}
