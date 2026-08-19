274830b525bca4ddf2ee5b1a1ec635c2c020ae91
Lachlan Leone <lachlan@starfluxgames.com>
2026-08-19 14:08:46 +1000

    Updated References
    - Updated references for 1.5.2
    - Recompiled for 1.5.2 fixing KL-2

--- Main.cs
+++ Main.cs
@@ -45,7 +45,7 @@ namespace KitchenLib
 		/// <summary>
 		/// The version of the mod.
 		/// </summary>
-		internal const string MOD_VERSION = "0.9.1";
+		internal const string MOD_VERSION = "0.9.2";
 
 		/// <summary>
 		/// The beta version of the mod.
