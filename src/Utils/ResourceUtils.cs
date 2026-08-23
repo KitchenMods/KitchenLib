8eea49ea5d426cf4f303e525d70f585a921c1085
Lachlan Leone <lachlan@starfluxgames.com>
2026-08-23 20:56:21 +1000

    Fixed resource caching

--- src/Utils/ResourceUtils.cs
+++ src/Utils/ResourceUtils.cs
@@ -34,16 +34,16 @@ namespace KitchenLib.Utils
 
 		public static Texture2D LoadTextureFromFile(string FilePath)
 		{
-			if (loadTextureFromFileCache.ContainsKey(VariousUtils.GetID("FilePath")))
+			if (loadTextureFromFileCache.ContainsKey(VariousUtils.GetID(FilePath)))
 			{
-				return loadTextureFromFileCache[VariousUtils.GetID("FilePath")];
+				return loadTextureFromFileCache[VariousUtils.GetID(FilePath)];
 			}
 			else
 			{
 				if (File.Exists(FilePath))
 				{
-					loadTextureFromFileCache.Add(VariousUtils.GetID("FilePath"), LoadTextureRaw(File.ReadAllBytes(FilePath)));
-					return loadTextureFromFileCache[VariousUtils.GetID("FilePath")];
+					loadTextureFromFileCache.Add(VariousUtils.GetID(FilePath), LoadTextureRaw(File.ReadAllBytes(FilePath)));
+					return loadTextureFromFileCache[VariousUtils.GetID(FilePath)];
 				}
 			}
 			return null;
